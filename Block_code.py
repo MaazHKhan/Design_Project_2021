dafcbc43571d1176436e8009c4b78e76c8de9db1
Maaz <maazkhan14@gmail.com>
2021-03-05 10:57:07 +0200

    Commit form desktop

--- Block_code.py
+++ Block_code.py
@@ -1,180 +1,180 @@
-# # -*- coding: utf-8 -*-
-# """
-# Created on Mon Oct 19 22:14:30 2020
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 19 22:14:30 2020
 
-# @author: maazk
-# """
+@author: maazk
+"""
 
-# import numpy as np
-# from matplotlib import pyplot as plt
-# from pylab import *
+import numpy as np
+from matplotlib import pyplot as plt
+from pylab import *
 
-# sample_rate = 2400000
+sample_rate = 2400000
 
-# file = open("data.bin", "r")
-# interleaved_data = np.fromfile(file, np.uint8)
-# file.close()
+file = open("data.bin", "r")
+interleaved_data = np.fromfile(file, np.uint8)
+file.close()
 
 
-# I_data_raw = interleaved_data[0:len(interleaved_data):2]
-# Q_data_raw = interleaved_data[1:len(interleaved_data):2]
-# I_samples = (I_data_raw-127.5)/127.5
-# Q_samples = (Q_data_raw-127.5)/127.5
-# complex_data = I_samples + 1j*Q_samples
+I_data_raw = interleaved_data[0:len(interleaved_data):2]
+Q_data_raw = interleaved_data[1:len(interleaved_data):2]
+I_samples = (I_data_raw-127.5)/127.5
+Q_samples = (Q_data_raw-127.5)/127.5
+complex_data = I_samples + 1j*Q_samples
 
-# block = complex_data[73656:99700]
+block = complex_data[73656:99700]
+
+plt.figure(1)
+plt.plot(abs(block))
+plt.grid()
+
+
+d=int(len(block))
 
 # plt.figure(1)
 # plt.plot(abs(block))
-# plt.grid()
+
+magnitude = np.abs(block)
+
+data_range = 2*d
+######### code for PSD plots ############
+data_x = np.arange(-sample_rate/2 ,sample_rate/2 ,sample_rate/data_range)
+
+freq_range = np.arange(0,sample_rate, sample_rate/data_range)
+fft = np.fft.fft(block ,data_range)
+psd_shift = np.fft.fftshift(fft)
+psd = abs(20*np.log10(np.abs(psd_shift)))
 
 
-# d=int(len(block))
-
-# # plt.figure(1)
-# # plt.plot(abs(block))
-
-# magnitude = np.abs(block)
-
-# data_range = 2*d
-# ######### code for PSD plots ############
-# data_x = np.arange(-sample_rate/2 ,sample_rate/2 ,sample_rate/data_range)
-
-# freq_range = np.arange(0,sample_rate, sample_rate/data_range)
-# fft = np.fft.fft(block ,data_range)
-# psd_shift = np.fft.fftshift(fft)
-# psd = abs(20*np.log10(np.abs(psd_shift)))
+######## Code for FFT Plots #########
+fft_1  = 20*np.log10(np.abs(np.fft.fft(np.abs(block ),sample_rate)))
 
 
-# ######## Code for FFT Plots #########
-# fft_1  = 20*np.log10(np.abs(np.fft.fft(np.abs(block ),sample_rate)))
-
-
-# ######## Code for fft bins  #########
-# fft_2  = 20*np.log10(np.abs(np.fft.fft(np.abs(block ))))
+######## Code for fft bins  #########
+fft_2  = 20*np.log10(np.abs(np.fft.fft(np.abs(block ))))
 
 
 
 
 
 
-# ######################################
-# ############# Resampling ############# 
-# ######################################
+######################################
+############# Resampling ############# 
+######################################
 
-# freq = 100000
-# angle = 2*0
+freq = 100000
+angle = 2*0
 
-# data_x_2 = np.linspace(0, d/sample_rate, d)
-# clock = np.cos((2*np.pi*freq*data_x_2+angle))
+data_x_2 = np.linspace(0, d/sample_rate, d)
+clock = np.cos((2*np.pi*freq*data_x_2+angle))
 
-# block_sync = []
-# for i in range (0,d-1):
-#     if (clock[i] > clock[i-1] and clock[i] > clock[i+1]):
-#         block_sync.append(block[i])  
-
-
-# unwrap = np.unwrap(np.angle(block_sync))
-# diff_angle = np.diff(unwrap)
-# sync = np.abs(block_sync[0:int(len(diff_angle))]) * (np.cos(diff_angle) + 0*1j*(np.sin(diff_angle)*1))
+block_sync = []
+for i in range (0,d-1):
+    if (clock[i] > clock[i-1] and clock[i] > clock[i+1]):
+        block_sync.append(block[i])  
 
 
-# decode = []
-# decode.append("0")
-# for j in range(0,int(len(sync))):
-#     if (sync[j] > 0.1):
-#         decode.append("0")   
-#     elif (sync[j] < -0.1):
-#         decode.append("1")
+unwrap = np.unwrap(np.angle(block_sync))
+diff_angle = np.diff(unwrap)
+sync = np.abs(block_sync[0:int(len(diff_angle))]) * (np.cos(diff_angle) + 0*1j*(np.sin(diff_angle)*1))
 
 
-# final = np.transpose(decode)
-
-# data = np.genfromtxt("blockdata.txt",dtype=int) #final_xx #
-
-# g = np.array([[1,0,0,0,1,1],[0,1,0,1,0,1],[0,0,1,1,1,1]]) #Genarator
-# h = np.array([[0,1,1,1,0,0],[1,0,1,0,1,0],[1,1,1,0,0,1]]) #Parity check matrix
-# ht = np.transpose(h)     
-# print("")                                 #Transpose of parity check matrix
-# print("Genarator =")
-# print(g)
-# print("")
-# print("Paraty check matrix =")
-# print(h)
-# print("")
-# print("Transpose of Paraty check matrix =")
-# print(ht)
+decode = []
+decode.append("0")
+for j in range(0,int(len(sync))):
+    if (sync[j] > 0.1):
+        decode.append("0")   
+    elif (sync[j] < -0.1):
+        decode.append("1")
 
 
-# data_r = np.reshape(data,(181,6))
+final = np.transpose(decode)
 
-# syndrome = (np.dot(data_r,ht))%2
+data = np.genfromtxt("blockdata.txt",dtype=int) #final_xx #
 
-# error_1 = [0,0,0,0,0,1]
-# error_2 = [0,0,0,0,1,0]
-# error_3 = [0,0,0,1,0,0]
-# error_4 = [0,1,0,0,0,0]
-# error_5 = [0,0,1,0,0,0]
-# error_6 = [0,0,0,0,0,0]
-# error_7 = [1,0,0,0,1,0]
-# error_8 = [1,0,0,0,0,0]
+g = np.array([[1,0,0,0,1,1],[0,1,0,1,0,1],[0,0,1,1,1,1]]) #Genarator
+h = np.array([[0,1,1,1,0,0],[1,0,1,0,1,0],[1,1,1,0,0,1]]) #Parity check matrix
+ht = np.transpose(h)     
+print("")                                 #Transpose of parity check matrix
+print("Genarator =")
+print(g)
+print("")
+print("Paraty check matrix =")
+print(h)
+print("")
+print("Transpose of Paraty check matrix =")
+print(ht)
 
-# error = []
 
-# for i in range(0,181):
-#     if (syndrome[i,0]==0 and syndrome[i,1]==0 and syndrome[i,2]==1):
-#         error.append(error_1)
-#     elif(syndrome[i,0]==0 and syndrome[i,1]==1 and syndrome[i,2]==0):
-#         error.append(error_2)
-#     elif(syndrome[i,0]==1 and syndrome[i,1]==0 and syndrome[i,2]==0):
-#         error.append(error_3)
-#     elif(syndrome[i,0]==1 and syndrome[i,1]==0 and syndrome[i,2]==1):
-#         error.append(error_4)
-#     elif(syndrome[i,0]==1 and syndrome[i,1]==1 and syndrome[i,2]==1):
-#         error.append(error_5)
-#     elif(syndrome[i,0]==0 and syndrome[i,1]==0 and syndrome[i,2]==0):
-#         error.append(error_6)
-#     elif(syndrome[i,0]==1 and syndrome[i,1]==1 and syndrome[i,2]==0):
-#         error.append(error_7)
-#     elif(syndrome[i,0]==0 and syndrome[i,1]==1 and syndrome[i,2]==1):
-#         error.append(error_8)
+data_r = np.reshape(data,(181,6))
 
-# code_word = (error + data_r)%2
+syndrome = (np.dot(data_r,ht))%2
 
-# data_out = []
+error_1 = [0,0,0,0,0,1]
+error_2 = [0,0,0,0,1,0]
+error_3 = [0,0,0,1,0,0]
+error_4 = [0,1,0,0,0,0]
+error_5 = [0,0,1,0,0,0]
+error_6 = [0,0,0,0,0,0]
+error_7 = [1,0,0,0,1,0]
+error_8 = [1,0,0,0,0,0]
 
-# for i in range(0,len(code_word)):
-#     data_out.append(code_word[i,0:3])
+error = []
 
-# out = np.transpose(data_out)
+for i in range(0,181):
+    if (syndrome[i,0]==0 and syndrome[i,1]==0 and syndrome[i,2]==1):
+        error.append(error_1)
+    elif(syndrome[i,0]==0 and syndrome[i,1]==1 and syndrome[i,2]==0):
+        error.append(error_2)
+    elif(syndrome[i,0]==1 and syndrome[i,1]==0 and syndrome[i,2]==0):
+        error.append(error_3)
+    elif(syndrome[i,0]==1 and syndrome[i,1]==0 and syndrome[i,2]==1):
+        error.append(error_4)
+    elif(syndrome[i,0]==1 and syndrome[i,1]==1 and syndrome[i,2]==1):
+        error.append(error_5)
+    elif(syndrome[i,0]==0 and syndrome[i,1]==0 and syndrome[i,2]==0):
+        error.append(error_6)
+    elif(syndrome[i,0]==1 and syndrome[i,1]==1 and syndrome[i,2]==0):
+        error.append(error_7)
+    elif(syndrome[i,0]==0 and syndrome[i,1]==1 and syndrome[i,2]==1):
+        error.append(error_8)
 
-# result = out.flatten('F')
+code_word = (error + data_r)%2
 
-# final_result = result[3:543]
+data_out = []
 
-# print("")
-# print("Data to decode:")
-# print(final_result)
+for i in range(0,len(code_word)):
+    data_out.append(code_word[i,0:3])
+
+out = np.transpose(data_out)
+
+result = out.flatten('F')
+
+final_result = result[3:543]
+
+print("")
+print("Data to decode:")
+print(final_result)
    
 
 
 
-# # plt.figure(8)
-# # plt.plot(np.real(sync),np.imag(sync),"o")
-# # plt.ylim(-1,1)
-# # plt.title("Carrier Synced(huffman)")
-# # plt.ylabel("In-Quadrature")
-# # plt.xlabel("In-Phase")
-# # plt.grid()
+# plt.figure(8)
+# plt.plot(np.real(sync),np.imag(sync),"o")
+# plt.ylim(-1,1)
+# plt.title("Carrier Synced(huffman)")
+# plt.ylabel("In-Quadrature")
+# plt.xlabel("In-Phase")
+# plt.grid()
 
 
-# # plt.figure(9)
-# # plt.plot(np.real(block_sync),np.imag(block_sync),"o")
-# # plt.ylim(-1,1)
-# # plt.title("Carrier Synced(huffman)")
-# # plt.ylabel("In-Quadrature")
-# # plt.xlabel("In-Phase")
-# # plt.grid()
+# plt.figure(9)
+# plt.plot(np.real(block_sync),np.imag(block_sync),"o")
+# plt.ylim(-1,1)
+# plt.title("Carrier Synced(huffman)")
+# plt.ylabel("In-Quadrature")
+# plt.xlabel("In-Phase")
+# plt.grid()
 
 print("Hello World")
 
